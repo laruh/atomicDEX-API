5b9b0f977334544f32470e994e845fe8786ca2f9
milerius <rmscastle@gmail.com>
2022-04-01 07:02:11 +0200

    wip(confirmation_status): add wait_for_confirmations change for z_coin

--- mm2src/coins/z_coin.rs
+++ mm2src/coins/z_coin.rs
@@ -740,7 +740,7 @@ impl MarketCoinOps for ZCoin {
         requires_nota: bool,
         wait_until: u64,
         check_every: u64,
-    ) -> Box<dyn Future<Item = (), Error = String> + Send> {
+    ) -> Box<dyn Future<Item = u64, Error = String> + Send> {
         utxo_common::wait_for_confirmations(self.as_ref(), tx, confirmations, requires_nota, wait_until, check_every)
     }
 
