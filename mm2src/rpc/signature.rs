00f413b5250df486f37ac47282371e8d40369ec4
Caglar Kaya <caglaryucekaya@gmail.com>
2022-04-13 13:38:15 +0300

    fix sign&verify message requests and responses

--- mm2src/rpc/signature.rs
+++ mm2src/rpc/signature.rs
@@ -4,7 +4,6 @@ use common::mm_error::MmError;
 use common::HttpStatusCode;
 use derive_more::Display;
 use http::StatusCode;
-use serde_json::{self as json, Value as Json};
 
 #[derive(Serialize, Display, SerializeErrorType)]
 #[serde(tag = "error_type", content = "error_data")]
@@ -20,8 +19,6 @@ pub enum SignatureError {
 pub enum VerificationError {
     #[display(fmt = "Invalid request: {}", _0)]
     InvalidRequest(String),
-    #[display(fmt = "Wallet error: {}", _0)]
-    WalletError(String),
 }
 
 #[derive(Serialize)]
@@ -29,11 +26,23 @@ pub struct SignatureResponse {
     signature: String,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct SignatureRequest {
+    coin: String,
+    message: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct VerificationRequest {
+    coin: String,
+    message: String,
+    signature: String,
+    address: String
+}
+
 #[derive(Serialize)]
 pub struct VerificationResponse {
     is_valid: bool,
-    address: String,
-    pubkey: String,
 }
 
 pub type SignatureResult<T> = Result<T, MmError<SignatureError>>;
@@ -53,55 +62,33 @@ impl HttpStatusCode for VerificationError {
     fn status_code(&self) -> StatusCode {
         match self {
             VerificationError::InvalidRequest(_) => StatusCode::BAD_REQUEST,
-            VerificationError::WalletError(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
 
-pub async fn sign_message(ctx: MmArc, req: Json) -> SignatureResult<SignatureResponse> {
-    let coin_name: String = json::from_value(req["coin"].clone())
-        .map_err(|_| SignatureError::InvalidRequest(String::from("No coin field")))?;
-    let message: String = json::from_value(req["message"].clone())
-        .map_err(|_| SignatureError::InvalidRequest(String::from("No message field")))?;
-    let coin = lp_coinfind_or_err(&ctx, &coin_name)
+pub async fn sign_message(ctx: MmArc, req: SignatureRequest) -> SignatureResult<SignatureResponse> {
+    let coin = lp_coinfind_or_err(&ctx, &req.coin)
         .await
         .map_err(|_| SignatureError::InvalidRequest(String::from("No such coin")))?;
-    let signature = coin.sign_message(&message).map_err(SignatureError::WalletError)?;
+    let signature = coin.sign_message(&req.message).map_err(SignatureError::WalletError)?;
     Ok(SignatureResponse { signature })
 }
 
-pub async fn verify_message(ctx: MmArc, req: Json) -> VerificationResult<VerificationResponse> {
-    let coin_name: String = json::from_value(req["coin"].clone())
-        .map_err(|_| VerificationError::InvalidRequest(String::from("No coin field")))?;
-    let message: String = json::from_value(req["message"].clone())
-        .map_err(|_| VerificationError::InvalidRequest(String::from("No message field")))?;
-    let signature: String = json::from_value(req["signature"].clone())
-        .map_err(|_| VerificationError::InvalidRequest(String::from("No signature field")))?;
-    let address: String = json::from_value(req["address"].clone())
-        .map_err(|_| VerificationError::InvalidRequest(String::from("No address field")))?;
-
-    let coin = lp_coinfind_or_err(&ctx, &coin_name)
+pub async fn verify_message(ctx: MmArc, req: VerificationRequest) -> VerificationResult<VerificationResponse> {
+    let coin = lp_coinfind_or_err(&ctx, &req.coin)
         .await
         .map_err(|_| VerificationError::InvalidRequest(String::from("No such coin")))?;
 
-    let validate_address_result = coin.validate_address(&address);
+    let validate_address_result = coin.validate_address(&req.address);
     if !validate_address_result.is_valid {
         return MmError::err(VerificationError::InvalidRequest(
             validate_address_result.reason.unwrap_or_else(|| "Unknown".to_string()),
         ));
     }
 
-    let address = coin.my_address().map_err(VerificationError::WalletError)?;
     let is_valid = coin
-        .verify_message(&signature, &message, &address)
+        .verify_message(&req.signature, &req.message, &req.address)
         .map_err(VerificationError::InvalidRequest)?;
-    let pubkey = coin
-        .get_public_key()
-        .map_err(|e| VerificationError::WalletError(e.to_string()))?;
 
-    Ok(VerificationResponse {
-        is_valid,
-        address,
-        pubkey,
-    })
+    Ok(VerificationResponse { is_valid })
 }
