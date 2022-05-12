96a5730920560369fc0b61ba39314a5dddef8a8d
laruh <oblomoff616@gmail.com>
2022-05-12 17:51:48 +0700

    removing code by replacing crates

--- mm2src/coins/z_transactions.rs
+++ mm2src/coins/z_transactions.rs
@@ -3,24 +3,18 @@ use zcash_primitives::zip32::ExtendedSpendingKey;
 use zcash_proofs::prover::LocalTxProver;
 use common::mm_ctx::MmArc;
 use common::mm_error::MmError;
-use crate::{AsyncMutex, Json, MarketCoinOps, UtxoActivationParams, ZTransaction};
+use crate::{AsyncMutex, Json, MarketCoinOps, SwapOps, UtxoActivationParams, ZTransaction};
 use crate::utxo::UtxoArc;
 use crate::z_coin::ZCoinBuildError;
 use std::sync::{Arc, Mutex, MutexGuard, Weak};
 use db_common::sqlite::rusqlite::{Connection, Error as SqliteError, Row, ToSql, NO_PARAMS};
 use std::path::PathBuf;
+
 use crate::z_coin::ZCoin;
+use crate::z_coin::ZCoinFields;
+use crate::z_coin::ZCoinBuilder;
 
 const DEX_FEE_OVK: OutgoingViewingKey = OutgoingViewingKey([7; 32]);
-
-pub struct ZCoinFields {
-    z_spending_key: ExtendedSpendingKey,
-    z_tx_prover: LocalTxProver,
-    /// Mutex preventing concurrent transaction generation/same input usage
-    z_unspent_mutex: AsyncMutex<()>,
-    /// SQLite connection that is used to cache Sapling data for shielded transactions creation
-    sqlite: Mutex<Connection>,
-}
 
 pub async fn z_coin_from_conf_and_params(
     ctx: &MmArc,
@@ -37,45 +31,17 @@ pub async fn z_coin_from_conf_and_params(
 fn init_db(sql: &Connection) -> Result<(), SqliteError> {
     const INIT_SAPLING_CACHE_TABLE_STMT: &str = "CREATE TABLE IF NOT EXISTS z_shielded (
         txid INTEGER NOT NULL PRIMARY KEY,
-        tx_hash VARCHAR(255) NOT NULL,
+        tx_hash BLOB NOT NULL,
         out_amount FLOAT
     );";
 
     sql.execute(INIT_SAPLING_CACHE_TABLE_STMT, NO_PARAMS).map(|_| ())
 }
 
-pub struct ZCoinBuilder<'a> {
-    ctx: &'a MmArc,
-    ticker: &'a str,
-    conf: &'a Json,
-    params: &'a UtxoActivationParams,
-    secp_priv_key: &'a [u8],
-    db_dir_path: PathBuf,
-    z_spending_key: ExtendedSpendingKey,
+fn encrypt_transactions(zcoin: &ZCoin, txbytes: &[u8]) {
+    let taker_fee_tx = zcoin.tx_enum_from_bytes(&txbytes).unwrap();
+    let a = zcoin.validate_fee();
 }
-
-impl<'a> ZCoinBuilder<'a> {
-    pub fn new(
-        ctx: &'a MmArc,
-        ticker: &'a str,
-        conf: &'a Json,
-        params: &'a UtxoActivationParams,
-        secp_priv_key: &'a [u8],
-        db_dir_path: PathBuf,
-        z_spending_key: ExtendedSpendingKey,
-    ) -> ZCoinBuilder<'a> {
-        ZCoinBuilder {
-            ctx,
-            ticker,
-            conf,
-            params,
-            secp_priv_key,
-            db_dir_path,
-            z_spending_key,
-        }
-    }
-}
-
 
 fn main() {
     // just checking what we can do with transactions
