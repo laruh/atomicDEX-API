1c3c935d8fa4581603f48ea031722b90ca8d3b7a
laruh <oblomoff616@gmail.com>
2024-09-05 17:04:22 +0700

    use TakerSwapCommand::SpendMakerPayment => check_maker_payment_spend_and_add_event(ctx, swap, saved).await in taker restart

--- mm2src/mm2_main/src/lp_swap/taker_restart.rs
+++ mm2src/mm2_main/src/lp_swap/taker_restart.rs
@@ -48,7 +48,7 @@ pub async fn get_command_based_on_maker_or_watcher_activity(
             Ok(None) => Ok(command),
             Err(e) => ERR!("Error {} when trying to find taker payment spend", e),
         },
-        TakerSwapCommand::SpendMakerPayment => Ok(command),
+        TakerSwapCommand::SpendMakerPayment => check_maker_payment_spend_and_add_event(ctx, swap, saved).await,
         TakerSwapCommand::ConfirmMakerPaymentSpend => Ok(command),
         TakerSwapCommand::PrepareForTakerPaymentRefund | TakerSwapCommand::RefundTakerPayment => {
             #[cfg(not(any(test, feature = "run-docker-tests")))]
